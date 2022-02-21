3e51cf6e78a3d2a53ba5eb0acae5208c4ad296b8
MONTANA-MELODY <99726482+MONTANA-MELODY@users.noreply.github.com>
2022-02-21 16:49:50 +0000

    Add files via upload
    fixed the statement print

--- Akin python.py
+++ Akin python.py
@@ -3,5 +3,5 @@ phonenumber = "082100415858"
 email = "oyebadeakinola@gmail.com"
 language = "python"
 
-info = name + " " + phonenumber + " " + email + " " + language
+info = "My name is " + name + " My phone number is " + phonenumber + " my email 'address's is " + email + " i speak " + language
 print (info)
