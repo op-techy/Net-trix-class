1a19eb61b1f8e0bc9a3a1e539a7386c8a3e0887d
chizzyokpe12 <99965579+chizzyokpe12@users.noreply.github.com>
2022-02-22 13:54:51 +0100

    Add files via upload
    I contributed using PHP

---
+++ chizzy.php
@@ -0,0 +1,11 @@
+<?php
+    $name = 'chizzy okpe';
+    $num = '09050047558';
+    $email = 'chizzyokpe12@gmail.com';
+    $lang = 'php';
+
+    $myIf = "I am $name my phone number is $num my email address is  $email and I speak $lang";
+
+    echo $myIf;
+    
+?>
