46935d4c90df3dc17fdc7c713952c2c1504ca2a5
MONTANA-MELODY <99726482+MONTANA-MELODY@users.noreply.github.com>
2022-02-22 13:35:22 +0000

    Add files via upload
    new

---
+++ akin the python 007.py
@@ -0,0 +1,8 @@
+myName = "i am Oyebade Akinola . E,"
+hobbies = "i love playing football and video games,"
+school = "i attended ekiti state university and i graduated in 2001."
+age = "i am not your mate o, i will be 30 years old in june"
+location = "i live in abuja and it is very hot here."
+
+info = myName + " " + hobbies + " " + school + " " + age + " " + location
+print (info)
